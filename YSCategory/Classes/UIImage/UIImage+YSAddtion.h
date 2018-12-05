e74dc41fcd1ef3a1de97b49adb877c2ceccf0d09
袁晓栋 <yuanxd25114@hsjry.com>
2018-12-05 11:06:39 +0800

    增加图片分类功能

--- YSCategory/Classes/UIImage/UIImage+YSAddtion.h
+++ YSCategory/Classes/UIImage/UIImage+YSAddtion.h
@@ -11,7 +11,38 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIImage (YSAddtion)
 
-+ (UIImage *)ys_imageWithSize:(CGSize)size color:(UIColor *)color;
+/**
+ 获取纯色图片
+
+ @param color 颜色
+ @return 图像
+ */
++ (UIImage *)ys_imageWithColor:(UIColor *)color;
+
+/**
+ 图片设置圆角
+
+ @return 图像
+ */
+- (UIImage *)ys_circleImage;
+
+
+/**
+ 通过图片Data数据第一个字节 来获取图片扩展名
+
+ @param data 数据
+ @return 类型
+ */
+- (NSString *)ys_contentTypeForImageData:(NSData *)data;
+
+
+/**
+ 获取图片某一点的颜色
+
+ @param point 位置
+ @return 颜色
+ */
+- (UIColor *)colorAtPoint:(CGPoint)point;
 
 @end
 
--- YSCategory/Classes/UIImage/UIImage+YSAddtion.m
+++ YSCategory/Classes/UIImage/UIImage+YSAddtion.m
@@ -9,15 +9,103 @@
 
 @implementation UIImage (YSAddtion)
 
-+ (UIImage *)ys_imageWithSize:(CGSize)size color:(UIColor *)color {
+//TODO:获取纯色图片
++ (UIImage *)ys_imageWithColor:(UIColor *)color {
     
-    UIGraphicsBeginImageContextWithOptions(size, 0, [UIScreen mainScreen].scale);
-    [color set];
-    UIRectFill(CGRectMake(0, 0, size.width, size.height));
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     
     return image;
+}
+
+//TODO:图片设置圆角
+- (UIImage *)ys_circleImage {
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0);
+    CGContextRef ctr = UIGraphicsGetCurrentContext();
+
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    CGContextAddEllipseInRect(ctr, rect);
+
+    CGContextClip(ctr);
+    
+    [self drawInRect:rect];
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
+//TODO:通过图片Data数据第一个字节 来获取图片扩展名
+- (NSString *)ys_contentTypeForImageData:(NSData *)data {
+    uint8_t c;
+    [data getBytes:&c length:1];
+    switch (c) {
+        case 0xFF:
+            return @"jpeg";
+        case 0x89:
+            return @"png";
+        case 0x47:
+            return @"gif";
+        case 0x49:
+        case 0x4D:
+            return @"tiff";
+        case 0x52:
+            if ([data length] < 12) {
+                return nil;
+            }
+            NSString *testString = [[NSString alloc] initWithData:[data subdataWithRange:NSMakeRange(0, 12)] encoding:NSASCIIStringEncoding];
+            if ([testString hasPrefix:@"RIFF"] && [testString hasSuffix:@"WEBP"]) {
+                return @"webp";
+            }
+            return nil;
+    }
+    return nil;
+}
+
+//TODO:获取图片某一点的颜色
+- (UIColor *)colorAtPoint:(CGPoint)point {
+    if (!CGRectContainsPoint(CGRectMake(0.0f, 0.0f, self.size.width, self.size.height), point)) {
+        return nil;
+    }
+    
+    NSInteger pointX = trunc(point.x);
+    NSInteger pointY = trunc(point.y);
+    CGImageRef cgImage = self.CGImage;
+    NSUInteger width = self.size.width;
+    NSUInteger height = self.size.height;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    int bytesPerPixel = 4;
+    int bytesPerRow = bytesPerPixel * 1;
+    NSUInteger bitsPerComponent = 8;
+    unsigned char pixelData[4] = { 0, 0, 0, 0 };
+    CGContextRef context = CGBitmapContextCreate(pixelData,
+                                                 1,
+                                                 1,
+                                                 bitsPerComponent,
+                                                 bytesPerRow,
+                                                 colorSpace,
+                                                 kCGImageAlphaPremultipliedLast |     kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+    CGContextSetBlendMode(context, kCGBlendModeCopy);
+    
+    CGContextTranslateCTM(context, -pointX, pointY-(CGFloat)height);
+    CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, (CGFloat)width, (CGFloat)height), cgImage);
+    CGContextRelease(context);
+    
+    CGFloat red   = (CGFloat)pixelData[0] / 255.0f;
+    CGFloat green = (CGFloat)pixelData[1] / 255.0f;
+    CGFloat blue  = (CGFloat)pixelData[2] / 255.0f;
+    CGFloat alpha = (CGFloat)pixelData[3] / 255.0f;
+    
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
 }
 
 @end
